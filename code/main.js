3dd17f25247bd1db9aef8325c85d482ab74950a9
Gitthuma <george.githuma@gmail.com>
2021-04-24 14:34:04 +0300

    Created a function statemenet

--- code/main.js
+++ code/main.js
@@ -3,6 +3,14 @@ let para = document.createElement('p');
 
 // Add your code here
 
+/*For the first task, you have to create a simple function — chooseName() — that prints a random name from the provided array (names) to the provided paragraph (para), and then run it once.*/
+
+/*Create the function statement*/
+
+function chooseName() {
+
+}
+
 // Don't edit the code below here!
 
 const section = document.querySelector('section');
